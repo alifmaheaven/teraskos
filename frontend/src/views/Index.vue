6f55e7db3b70731ff5b7c768231c5ffae3021278
Alif Maheaven <alifmaheaven2001@gmail.com>
2021-05-10 16:33:08 +0700

    ftestimoni about and our team

--- frontend/src/views/Index.vue
+++ frontend/src/views/Index.vue
@@ -65,80 +65,26 @@
         </div>
       </div>
 
+      <div class="section p-3">
+        <div class="container">
+          <TestimoniTerasKos />
+        </div>
+      </div>
+
+      <div class="section p-3">
+        <div class="container">
+          <AboutTerasKos />
+        </div>
+      </div>
+
+      <div class="section p-3">
+        <div class="container">
+          <OurTeamTeraskos />
+        </div>
+      </div>
+
       <div class="section section-download" id="downloadSection">
         <div class="container">
-          <div class="md-layout text-center">
-            <div class="md-layout-item md-size-66 md-small-size-100 mx-auto">
-              <h2>Do you love this UI Kit?</h2>
-              <h4>
-                Cause if you do, it can be yours for FREE. Hit the buttons below
-                to navigate to our website where you can find the kit. Our
-                friends from
-                <a
-                  href="https://themeisle.com/?utm_campaign=mkfree-hestia&amp;utm_source=creativetim&amp;utm_medium=website"
-                  target="_blank"
-                  >ThemeIsle</a
-                >
-                created a Wordpress Theme which can be also downloaded for free.
-                Start a new project or give an old Bootstrap project a new look!
-              </h4>
-            </div>
-            <div class="md-layout-item md-size-50 md-small-size-100 mx-auto">
-              <md-button
-                href="https://www.creative-tim.com/product/material-kit"
-                target="_blank"
-                class="md-success md-lg"
-                ><i class="fab fa-html5"></i> Free HTML Download</md-button
-              >
-              <md-button
-                href="https://themeisle.com/themes/hestia/?utm_campaign=mkfree-hestia&amp;utm_source=creativetim&amp;utm_medium=website"
-                class="md-success md-lg"
-                target="_blank"
-                ><i class="fab fa-wordpress"></i> Wordpress Theme</md-button
-              >
-            </div>
-          </div>
-          <br /><br />
-
-          <div class="md-layout text-center">
-            <div class="md-layout-item md-size-66 md-small-size-100 mx-auto">
-              <h2>Want more?</h2>
-              <h4>
-                We've just launched
-                <a
-                  href="https://www.creative-tim.com/product/vue-material-dashboard-pro?ref=utp-mk-demos"
-                  target="_blank"
-                  >Vue Material Dashboard PRO</a
-                >
-                and
-                <a
-                  href="https://www.creative-tim.com/product/vue-material-kit-pro?ref=utp-vmk-demos"
-                  target="_blank"
-                  >Vue Material Kit PRO</a
-                >. Every product has a huge number of components, sections and
-                example pages. Start Your Development With A Badass Bootstrap UI
-                Kit inspired by Material Design.
-              </h4>
-            </div>
-            <div class="md-layout-item md-size-50 md-small-size-100 mx-auto">
-              <div class="btn-container">
-                <md-button
-                  href="https://www.creative-tim.com/product/vue-material-kit-pro?ref=utp-vmk-demos"
-                  class="md-button md-success md-lg md-upgrade"
-                  target="_blank"
-                  ><md-icon>unarchive</md-icon>Vue Material Kit PRO</md-button
-                >
-                <md-button
-                  href="https://www.creative-tim.com/product/vue-material-dashboard-pro?ref=utp-vmd-demos"
-                  class="md-button md-success md-lg md-upgrade"
-                  target="_blank"
-                  ><md-icon>unarchive</md-icon>Vue Material Dashboard
-                  PRO</md-button
-                >
-              </div>
-            </div>
-          </div>
-
           <div class="sharing-area text-center">
             <div class="md-layout">
               <div class="md-layout-item">
@@ -170,13 +116,19 @@ import SearchKosAtas from "./components/homepage/SearchKosAtas";
 import GuideForRegistration from "./components/homepage/GuideForRegistration";
 import FasilityOnTerasKos from "./components/homepage/FasilityOnTerasKos"
 import RecomendKos from "./components/homepage/RecomendKos"
+import AboutTerasKos from "./components/homepage/AboutTerasKos"
+import TestimoniTerasKos from "./components/homepage/TestimoniTerasKos"
+import OurTeamTeraskos from "./components/homepage/OurTeamTeraskos"
 
 export default {
   components: {
     SearchKosAtas,
     GuideForRegistration,
     FasilityOnTerasKos,
-    RecomendKos
+    RecomendKos,
+    AboutTerasKos,
+    TestimoniTerasKos,
+    OurTeamTeraskos
   },
   name: "index",
   bodyClass: "index-page",
---
+++ frontend/src/views/components/homepage/AboutTerasKos.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="row">
+      <div class="md-layout text-center">
+        <div class="md-layout-item md-size-66 md-small-size-100 mx-auto">
+          <h2>Visi Kami</h2>
+          <h4>
+            “To become the biggest property management platform in Indonesia through standardized product quality with affordable price, fast and friendly services by optimizing creative R&D and technology”.
+          </h4>
+          <h4>~ M. Rakhmat Setiawan : Founder & CEO</h4>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {},
+  data() {
+    return {
+      form: {},
+      fasility:[]
+    };
+  },
+  computed:{},
+  methods: {}
+};
+</script>
